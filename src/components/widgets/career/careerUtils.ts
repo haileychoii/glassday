00be48e7ad7d80a8ab1b088840e83a0c6bc32a13
Junhee Choi <128196297+haileychoii@users.noreply.github.com>
2026-06-22 08:32:41 +0900

    Create careerUtils.ts

---
+++ src/components/widgets/career/careerUtils.ts
@@ -0,0 +1,10 @@
+import type{
+  CareerAttachment, 
+  CareerAttachmentType,
+  CareerItem,
+  CareerPriority,
+  CareerStage,
+  CareerStageStatus,
+  CoverLetterItem,
+  CoverLetterStatus,
+}from "../../../types/dashboard;
